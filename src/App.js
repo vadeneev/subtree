e71207bfc23906ffe61b487fd65b42732676d821
Daniil Vadeneev <dvadeneev@griddynamics.com>
2019-07-19 18:01:55 +0400

    appends child2 from subtree

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Child from './modules/child1/src/component/Child/Child';
+import Child2 from './modules/child2/src/components/Child2/Child2';
 
 function App() {
   return (
     <div className="App">
       TOP APP
       <Child />
+      <Child2 />
     </div>
   );
 }
