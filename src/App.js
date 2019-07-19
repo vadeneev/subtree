db732427ec1423a27bd541261c567daf3e172c55
Daniil Vadeneev <dvadeneev@griddynamics.com>
2019-07-19 17:42:35 +0400

    child branch commit 1

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Child from './component/Child/Child';
+
+function App() {
+  return (
+    <div className="App">
+      Child's app
+      <Child />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/Child/Child.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Child() {
+  return (
+    <div className="Child">
+    Child repo 1 version 1
+    </div>
+  );
+}
+
+export default Child;
